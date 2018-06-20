2f0afeaa2ee2323c1c498ca8a034b5bba6c9ad1a
Makhles <mr.lange85@gmail.com>
2018-06-20 16:56:01 -0300

    RF04 implementado

--- ctps.sol
+++ ctps.sol
@@ -6,16 +6,21 @@ pragma solidity ^0.4.24;
 // Carteira de Trabalho e Previdência Social
 contract CTPS {
 
-    // Dados do empregado
     address private empregado;
     address private previdenciaSocial;
     address private dadosPessoais;
+
+    address[] private solicitacoes;
+    address[] private contratos;
 
     modifier onlyBy(address _quem) {
         require(_quem == msg.sender,
         "Acesso negado.");
         _;
     }
+
+    // Evento que indica que um empregador deseja firmar um contrato com o dono da carteira
+    event SolicitacaoContrato(address _contrato);
 
     // RF01 - 0x0c6c57e6e93725646e60bb23308a054e8870aa9c
     constructor(address _empregado, uint8 _dummy, address _dadosPessoais) public {
@@ -27,6 +32,12 @@ contract CTPS {
     // RF02 e RF03 - a alteração de dados poderá ser feita somente pela Previdência Social
     function alterarDadosPessoais(address _dadosPessoais) public onlyBy(previdenciaSocial) {
         dadosPessoais = _dadosPessoais;
+    }
+
+    // RF04
+    function solicitarFirmaContrato(address _contrato) public {
+        solicitacoes.push(_contrato);
+        emit SolicitacaoContrato(_contrato);
     }
 
     // ---------
